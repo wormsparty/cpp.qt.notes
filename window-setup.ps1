7eec415ad93f26a5b8abd5572fb382904ccfc929
mob <marc-olivier.bloch@elca.ch>
2023-11-16 14:32:28 +0100

    Add note for windeployqt'

--- window-setup.ps1
+++ window-setup.ps1
@@ -1,2 +1,4 @@
+echo "Don't forget you'll need to run 'windeployqt' the first time."
+
 [system.Diagnostics.Process]::Start("msedge","https://www.qt.io/download-qt-installer-oss")
 [system.Diagnostics.Process]::Start("msedge","https://marketplace.visualstudio.com/items?itemName=TheQtCompany.QtVisualStudioTools2022")
