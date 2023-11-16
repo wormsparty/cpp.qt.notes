97252e019baea261177c6c116d16da2321ab6c64
wormsparty <wormsparty@gmail.com>
2023-11-16 21:12:23 +0100

    Basic layout, remove .vscode from repository

--- debian-setup.sh
+++ debian-setup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-sudo apt-get install clang build-essential qt6-base-dev
+sudo apt-get install clang build-essential qt6-base-dev 
+# For development: designer-qt6
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <QApplication>
+#include <QListView>
+#include <QTextEdit>
 #include <QPushButton>
+#include <QGridLayout>
+#include <QStringListModel>
 
 #if defined(_WIN64) || defined(_WIN32)
 
@@ -15,8 +19,27 @@ int WinMain()
 
 #endif
 
-	QPushButton button ("Hello world !");
-	button.show();
+	QGridLayout layout;
+	QListView left;
+	QStringListModel model;
+
+    QStringList list;
+    list << "Clair de Lune" << "Reverie" << "Prelude";
+    model.setStringList(list);
+
+    left.setModel(&model);
+	left.setMaximumWidth(200);
+
+	layout.addWidget(&left, 0, 0, 1, 1);
+
+	QTextEdit editor;
+	editor.setPlainText("I am text.");	
+	layout.addWidget(&editor, 0, 1, 1, 1);
+	
+    QWidget widget;
+    widget.setLayout(&layout);
+    widget.setWindowTitle("Hi !");
+    widget.show();
 
 	return app.exec();
 }
