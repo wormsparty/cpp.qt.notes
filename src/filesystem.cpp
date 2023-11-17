7f2ceb9836ddf428dcd50ca688c05a6902136b08
wormsparty <wormsparty@gmail.com>
2023-11-17 15:01:16 +0100

    Add filesystem sample

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -1,16 +1,26 @@
 #include <filesystem>
+#include <fstream>
+#include <iostream>
 
 int main() {
-    const auto bigFilePath {"bigFileToCopy"};
+    const std::filesystem::path path{"/home/mob/mytest"};
 	
-    if (std::filesystem::exists(bigFilePath)) {
-        const auto bigFileSize {std::filesystem::file_size(bigFilePath)};
-	
-        std::filesystem::path tmpPath {"/tmp"};
-        
-        if (std::filesystem::space(tmpPath).available > bigFileSize) {
-		    std::filesystem::create_directory(tmpPath.append("example"));
-		    std::filesystem::copy_file(bigFilePath, tmpPath.append("newFile"));
-	    }
-	}
+    if (!std::filesystem::exists(path)) {
+	    std::filesystem::create_directory(path);
+    } else if (!std::filesystem::is_directory(path)) {
+        throw "Failed to create folder: is a file.";
+    }
+
+    std::ofstream out{path/"file.txt"};
+    out << "I'm testing";
+    out.close();
+
+    std::ifstream in{path/"file.txt"};
+    std::stringstream strstream;
+    strstream << in.rdbuf();
+    std::string content = strstream.str();
+
+    std::cout << "I read: " << content << std::endl;
+
+    return 0;
 }
