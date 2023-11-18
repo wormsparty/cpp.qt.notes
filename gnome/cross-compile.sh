c7079b0e7f9a53d297523b9e23a08f06e4f4c214
wormsparty <wormsparty@gmail.com>
2023-11-18 02:40:58 +0100

    Move to cross.meson, add static linking

--- gnome/cross-compile.sh
+++ gnome/cross-compile.sh
@@ -2,27 +2,14 @@
 
 sudo apt-get install mingw-w64 python3-pip python3-venv wine bison flex
 
-python3 -m venv meson
-meson/bin/pip3 install meson
-
-if [ ! -f spec.txt ]; then
-	cat << EOT > spec.txt
-[binaries]
-c = 'x86_64-w64-mingw32-gcc'
-cpp = 'x86_64-w64-mingw32-g++'
-ar = 'x86_64-w64-mingw32-ar'
-strip = 'x86_64-w64-mingw32-strip'
-windres = 'x86_64-w64-mingw32-windres'
-exe_wrapper = 'wine'
-
-[host_machine]
-system = 'windows'
-cpu_family = 'x86_64'
-cpu = 'x86_64'
-endian = 'little'
-EOT
+if [ ! -d meson ]; then
+	python3 -m venv meson
+	meson/bin/pip3 install meson
 fi
 
-meson/bin/meson setup --cross-file spec.txt build-mingw
+meson/bin/meson setup --cross-file cross.meson build-mingw
 meson/bin/meson compile -C build-mingw
-meson/bin/meson dist -C build-mingw
+
+rm -fr ./bin
+mkdir -p ./bin
+DESTDIR=./../bin meson/bin/meson install -C build-mingw
