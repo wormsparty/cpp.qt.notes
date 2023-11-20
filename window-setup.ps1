c905d8691531612dfa65f8f5e096eb8c228de562
mob <marc-olivier.bloch@elca.ch>
2023-11-20 08:57:50 +0100

    Update documentation

--- window-setup.ps1
+++ window-setup.ps1
@@ -1,4 +1,7 @@
-echo "Don't forget you'll need to run 'windeployqt' after the first build."
-
 [system.Diagnostics.Process]::Start("msedge","https://www.qt.io/download-qt-installer-oss")
 [system.Diagnostics.Process]::Start("msedge","https://marketplace.visualstudio.com/items?itemName=TheQtCompany.QtVisualStudioTools2022")
+ 
+echo "Use either Qt Creator or Visual Studio."
+echo "For Visual Studio:"
+echo "1. Open Visual Studio with the Qt extension installed."
+echo "2. Extensions > Qt VS Tools > Open Qt Project File (.pro)"
